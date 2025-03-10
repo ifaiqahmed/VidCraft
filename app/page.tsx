ad88903e858be8cddf3315f5a3d2a7c1676022df
Faiq Ahmed <faiqaws1234@gmail.com>
2025-03-10 12:58:07 +0100

    homepage completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import BgGradient from '@/components/common/gradient'
 import Banner from '@/components/home/Banner'
+import HowItWorks from '@/components/home/HowItWorks'
+import Pricing from '@/components/home/Pricing'
 import { Button } from '@/components/ui/button'
 import { Dot } from 'lucide-react'
 import React from 'react'
@@ -14,12 +16,20 @@ const page = () => {
 <Dot className='text-purple-400' ></Dot>
 <Dot className='text-purple-400' ></Dot>
 </div>
-{/* <Divider/>
 <HowItWorks/>
-<Divider/>  
+<div className='flex items-center justify-center' >
+<Dot className='text-purple-400' ></Dot>
+<Dot className='text-purple-400' ></Dot>
+<Dot className='text-purple-400' ></Dot>
+</div>
 <Pricing/>  
-<Divider/>
-<Footer/> */}
+<div className='flex items-center justify-center' >
+<Dot className='text-purple-400' ></Dot>
+<Dot className='text-purple-400' ></Dot>
+<Dot className='text-purple-400' ></Dot>
+</div>
+<footer className='bg-gray-200/20 flex h-20 py-20 px-12 z-20 relative overflow-hidden'><p>
+  All Rights Reserved, {new Date().getFullYear()}</p></footer>
    </main>
   )
 }
---
+++ lib/assets.ts
@@ -0,0 +1,19 @@
+export const plansMap = [
+    {
+      id: "basic",
+      name: "Basic",
+      description: "Get started with SpeakEasy!",
+      price: "10",
+      items: ["3 Blog Posts", "3 Transcription"],
+      paymentLink: "",
+
+    },
+    {
+      id: "pro",
+      name: "Pro",
+      description: "All Blog Posts, let’s go!",
+      price: "19.99",
+      items: ["Unlimited Blog Posts", "Unlimited Transcriptions"],
+      paymentLink: "",
+    },
+  ];
